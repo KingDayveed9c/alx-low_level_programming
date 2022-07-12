7a449ade2e61237509a4e5752ce9c5c95c2f90ac
Adejumo David <davidadejumo@gmail.com>
2022-07-12 16:13:55 -0700

    C-pointers_and_array

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
-* main - a function that takes a
-*
-* pointer to an
-*
-* int as parameter and updates the value to 98
+* reset_to_98 - reset argument to 98
+* @n: pointer - Entry point
 *
 * Return: nothing
 */
