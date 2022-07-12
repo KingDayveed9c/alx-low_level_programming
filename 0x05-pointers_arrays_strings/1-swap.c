4d76d48101b25518c4e732dadaaf9499a14b241a
Adejumo David <davidadejumo@gmail.com>
2022-07-12 16:43:48 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*swap_int(int *a, int *b)- swaps two integers
+*@a: pointer
+*@b: pointer
+*
+*Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
