2228ba83516b51b4c599d71907c3a74cb11c2e98
Adejumo David <davidadejumo@gmail.com>
2022-07-12 18:33:18 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - entry point
+ * @str: value to be evaluated.
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int len = 0;
+int l = 0;
+char *y = str;
+int z;
+
+while (*y != '\0')
+{
+y++;
+len++;
+}
+l = len - 1;
+for (z = 0 ; z <= l ; z++)
+{
+if (z % 2 == 0)
+{
+_putchar(str[z]);
+}
+}
+_putchar('\n');
+}
