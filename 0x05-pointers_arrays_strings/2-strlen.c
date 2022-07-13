9cab0339e3353ce9e19afa82baa4dcbe6b4866d7
Adejumo David <davidadejumo@gmail.com>
2022-07-12 17:16:49 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strlen - string length
+*@s: pointer
+*
+*Return: length of string
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+return (0);
+}
