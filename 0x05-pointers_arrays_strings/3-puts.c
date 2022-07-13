d6e2c83c16834b29bd48d1306fb08d6c2c08ef94
Adejumo David <davidadejumo@gmail.com>
2022-07-12 17:50:26 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+* _puts - prints a string to stdout
+* @str: value to print
+* Return: void
+*/
+void _puts(char *str)
+{
+int l = 0;
+
+while (*(str + l) != '\0')
+{
+_putchar(str[l]);
+l++;
+}
+_putchar('\n');
+}
