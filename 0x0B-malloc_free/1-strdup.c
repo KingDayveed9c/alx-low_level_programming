fdf3fa6eb6a3b6fabc6a6348e8b0413cdba562da
David Adejumo <davidadejumo@gmail.com>
2022-07-26 13:38:49 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 	char *strout;
 	unsigned int i, j;
 
-if (str == NULL)
+	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
@@ -22,7 +22,7 @@ strout = (char *)malloc(sizeof(char) * (i + 1));
 	if (strout == NULL)
 		return (NULL);
 
-for (j = 0; j <= i; j++)
+	for (j = 0; j <= i; j++)
 		strout[j] = str[j];
 
 return (strout);
