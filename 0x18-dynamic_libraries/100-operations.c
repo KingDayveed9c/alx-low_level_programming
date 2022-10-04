5cf159927774e4c570f1c3adf35aff2f2ca039bc
David Adejumo <davidadejumo@gmail.com>
2022-10-03 17:17:00 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* main - addition function
+*@a: operand
+*@b: operand
+* Return: sum of numbers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
